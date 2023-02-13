7aa5616bbf537d99b4efaf6e0534807275b6794e
Kusák Jaroslav <jarekusak@seznam.cz>
2023-02-13 20:31:35 +0000

    test1

--- 01/project_name.py
+++ 01/project_name.py
@@ -1,7 +1,42 @@
+import os
+
 #!/usr/bin/env python3
 
 def main():
-    print('project-name')
+    nazvy = ["readme.md", "README", "readme"]
+    pom = False
+    nazev = None
+    try:
+        for i in range(len(nazvy)):
+            if os.path.isfile(nazvy[i]):
+                nazev = nazvy[i]
+                pom = True
+                break
+        if pom:
+            with open(nazev, mode="r", encoding="utf-8") as f:
+                line = f.readline()
+                while(line != ""): # čte po řádcích, dokud neskončí soubor
+                    
+                    if line[0] == "\n":
+                        line = f.readline()
+                        continue
+                    elif line[0] == "":
+                        line = f.readline()
+                        continue
+                    elif line.strip().lstrip('# ') == '':
+                        line = f.readline()
+                        continue
+                    else:
+                        s = line.replace("#", " ")  
+                        s = s.strip()
+                        print(s)
+                        break
+                
+
+        else:
+            print(os.path.basename(os.getcwd()))
+    except:
+        print(os.path.basename(os.getcwd()))
 
 if __name__ == '__main__':
     main()
