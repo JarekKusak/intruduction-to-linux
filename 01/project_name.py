124f3387fe5c11bb7e1cf323cb008af27439f6c3
Kusák Jaroslav <jarekusak@seznam.cz>
2023-02-13 20:56:06 +0000

    Update 01/project_name.py

--- 01/project_name.py
+++ 01/project_name.py
@@ -1,36 +1,41 @@
-import os
-
 #!/usr/bin/env python3
+
+import os
 
 def main():
     nazvy = ["README.md", "readme.md", "README", "readme"]
     pom = False
     nazev = None
+    semaf = True
     try:
         for i in range(len(nazvy)):
+            if semaf == False:
+                break
             if os.path.isfile(nazvy[i]):
                 nazev = nazvy[i]
                 pom = True
-                break
-        if pom:
-            with open(nazev, mode="r", encoding="utf-8") as f:
-                line = f.readline()
-                while(line != ""): # čte po řádcích, dokud neskončí soubor
-                    
-                    if line[0] == "\n":
-                        line = f.readline()
+            if pom:
+                with open(nazev, mode="r", encoding="utf-8") as f:
+                    if f.readline(1) == "":
                         continue
-                    elif line[0] == "":
-                        line = f.readline()
-                        continue
-                    elif line.strip().lstrip('# ') == '':
-                        line = f.readline()
-                        continue
-                    else:
-                        s = line.replace("#", " ")  
-                        s = s.strip()
-                        print(s)
-                        break
+                    line = f.readline()
+                    while(line != ""): # čte po řádcích, dokud neskončí soubor
+                        
+                        if line[0] == "\n":
+                            line = f.readline()
+                            continue
+                        elif line[0] == "":
+                            line = f.readline()
+                            continue
+                        elif line.strip().lstrip('# ') == '':
+                            line = f.readline()
+                            continue
+                        else:
+                            s = line.replace("#", " ")  
+                            s = s.strip()
+                            print(s)
+                            semaf = False
+                            break
                 
 
         else:
